e779c8d901e954b20f0ecd9781ade1525b7d3a3a
Sian Jose <sian3jose@gmail.com>
2024-10-23 13:19:32 -0400

    add decode function

--- lab_six.py
+++ lab_six.py
@@ -2,19 +2,24 @@ def encode(to_encode): #Grace Wang
     holder = ""
     for i in to_encode:
         holder +=str(int(i)+3)
-    print(holder)
+    print("Your password has been encoded and stored!")
     return holder
 
+def decode(encoded):
+    decoded = ""
+    for i in encoded:
+	    decoded += str(int(i)-3)
+    print(f"The encoded password is {encoded}, and the original password is {decoded}")
+    return decoded
 
 if __name__ == '__main__':
     while True:
-        print("Menu\n-------------\n1. Encode\n2. Decode\n3.Quit")
+        print("\nMenu\n-------------\n1. Encode\n2. Decode\n3.Quit")
         option = float(input("Please enter an option: "))
         if option == 1:
             encode_info = input("Please enter your password to encode: ")
-            encode(encode_info)
+            encoded_password = encode(encode_info)
         elif option == 2:
-            #decode function here
-            pass
+            decode(encoded_password) 
         elif option == 3:
             break
